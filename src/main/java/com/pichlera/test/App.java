bfb2cbd28a66f6f7e5d5843a7c8d5e92742305f1
Andreas Pichler <andreaspichler@tsn.at>
2018-11-07 12:38:17 +0100

    add maven Project

---
+++ src/main/java/com/pichlera/test/App.java
@@ -0,0 +1,13 @@
+package com.pichlera.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
